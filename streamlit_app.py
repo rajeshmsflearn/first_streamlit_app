a2dfa8f253a3f7c4b495bdc4fbeff2e6de10bcf1
rajeshmsflearn <130906894+rajeshmsflearn@users.noreply.github.com>
2023-04-16 13:22:12 +0100

    Update streamlit_app.py
    Update the title message

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,2 @@
 import streamlit
-streamlit.title('I am healthy and wealthy. What a great feeling !!!')
+streamlit.title('I am healthy and wealthy. What a great feeling !!!. Kudos to Baru for providing me healthy food daily :)')
