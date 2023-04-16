a7d2d36320ccc03262df752c3a8a7b4b95abbfa9
rajeshmsflearn <130906894+rajeshmsflearn@users.noreply.github.com>
2023-04-16 13:24:43 +0100

    Update streamlit_app.py
    Added lines 3,4,5 breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title('I am healthy and wealthy. What a great feeling !!!. Kudos to Baru for providing me healthy food daily :)')
+streamlit.header('Breakfast Menu')
+streamlit.text('Dosa, Vada, Idly, Sambar')
+streamlit.text('Poori, Sagu, Paneer Butter masala')
