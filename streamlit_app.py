4d67036ff542ce89fa784fa63f3d25d7637caff9
rajeshmsflearn <130906894+rajeshmsflearn@users.noreply.github.com>
2023-04-16 13:50:26 +0100

    Update streamlit_app.py
    added code 9,10,11 to import panadas, create dataframe by reading csv and display using streamlit function dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
-import pandas
 streamlit.title('I am healthy and wealthy. What a great feeling !!!. Kudos to Baru for providing me healthy food daily :)')
 streamlit.header('🥗 🥣 Breakfast Menu')
 streamlit.text('🐔 Dosa, Vada, Idly, Sambar')
 streamlit.text('🍞 Poori, Sagu, Paneer Butter masala')
 streamlit.text('🥑 Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlist.dataframe(my_fruit_list)
